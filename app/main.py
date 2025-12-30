1636b7673f443a9eb699b4aa8fd5e6968f8be11d
BenMcCormack <g00416547@atu.ie>
2025-12-30 12:14:17 +0000

    Post, Delete and Heaalth added

--- app/main.py
+++ app/main.py
@@ -21,3 +21,28 @@ def add_user(user: User):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id already exists")
     users.append(user)
     return user
+
+@app.put("/api/users/{user_id}")
+def replace_user(user_id: int, user: User):
+    for index, u in enumerate(users):
+        if u.user_id == user_id:
+            users[index] = user
+            return user
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User not found"
+    )
+
+@app.delete("/api/users/{user_id}")
+def delete_user(user_id: int, user: User):
+    for u in users:
+        if u.user_id == user_id:
+            users.remove(user)
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User not found"
+    )
+ 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
